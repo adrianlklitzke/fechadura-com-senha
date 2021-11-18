0f346648f347ffbe2e62b5dcbf3f1f0e064ff661
ympellenz <82010324+ympellenz@users.noreply.github.com>
2021-11-18 17:04:28 -0300

    Add files via upload
    Primeiro commit

---
+++ projeto_final/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
